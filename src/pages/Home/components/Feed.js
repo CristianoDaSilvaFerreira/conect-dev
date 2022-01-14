ecebe1ed81f2c46d69db2073dfe0ebeeeaf0c9e5
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-01-14 16:36:27 -0300

    Estilizando o Feed primeira parte

--- src/pages/Home/components/Feed.js
+++ src/pages/Home/components/Feed.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+
+import PostCard from "../../../components/PostCard";
+
+const useStyles = makeStyles ((theme) =>({
+    root: {
+    },
+}));
+
+const posts = [
+    { 
+        id: 1, 
+        author: {
+            id: 1,
+            name: 'Cristiano Ferreira',
+            username: 'cristianoferreira',
+            avatar: '/images/avatars/avatar_1.jpeg'
+        },
+        title: 'Criando um App do zero utilizando React.js',
+        date: 'Janeiro 13, 2022',
+        description: 'Fala pessoal! Qual o framework favorito de vcs?',
+        hashtags: '#dotnet, #javascript, #reactjs, #developer',
+        image: '/images/posts/post9.jpeg'
+    },
+    { 
+        id: 2, 
+        author: {
+            id: 1,
+            name: 'Cristiano Ferreira',
+            username: 'cristianoferreira',
+            avatar: '/images/avatars/avatar_1.jpeg'
+        },
+        title: 'Criando um App do zero utilizando React.js',
+        date: 'Janeiro 13, 2022',
+        description: 'Fala pessoal! Qual o framework favorito de vcs?',
+        hashtags: '#dotnet, #javascript, #reactjs, #developer',
+        image: '/images/posts/post8.png'
+    },
+];
 
 function Feed() {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            Feed
+        <div className={classes.root}>
+            {
+                posts.map(post => (
+                    <PostCard key={post.id} post={post} />
+                ))
+            }
         </div>
     )
 }
