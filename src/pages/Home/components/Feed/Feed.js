f18f3bd74f484719e789293d684c177d87df3bd1
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2022-03-22 15:04:16 -0300

    :up:

--- src/pages/Home/components/Feed/Feed.js
+++ src/pages/Home/components/Feed/Feed.js
@@ -1,7 +1,27 @@
 import React from "react";
+import PostCard from "../../../../components/PostCard";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useSyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+const posts = [
+  { id: 1, description: "Teste 1" },
+  { id: 2, description: "Teste 2" },
+  { id: 3, description: "Teste 3" },
+];
 
 function Feed() {
-  return <div>Feed</div>;
+  const classes = useSyles();
+  return (
+    <div className={classes.root}>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default Feed;
